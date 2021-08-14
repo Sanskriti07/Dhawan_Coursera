2125fe7ff84c0dc15ba3ef3ac0f369b93f219609
Sanskriti <sanskritidhawan07@gmail.com>
2021-08-15 00:28:54 +0530

    adding sources file

--- sources.mk
+++ sources.mk
@@ -10,8 +10,30 @@
 #*****************************************************************************
 
 # Add your Source files to this variable
-SOURCES =
+ifeq($(PLATFORM),HOST)
+	SOURCES =	main.c \
+			memory.c \
+
+else
+	SOURCES =		\
+			main.c \
+			memory \
+			system_msp432p401r.c \
+			interrupts_msp432p401r_gcc.c \
+			startup_msp432p401r_gcc.c
+endif
 
 # Add your include paths to this variable
-INCLUDES = 
+ifeq($(PLATFORM),HOST)
+	INCLUDES =		\
+			-I../	\
+       			-I../include/common \
+			-I../include/CMSIS  \
+
+else
+	INCLUDES =		\
+			-I../include/common \
+			-I../include/msp432 \
+			-I../include/CMSIS  
+endif
 
